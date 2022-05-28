f798118a8976e669e9430abafa5bf36095be6798
kcl215 <98423401+kcl215@users.noreply.github.com>
2022-05-28 22:30:32 +0900

    section4-75 finish

--- memo/index.php
+++ memo/index.php
@@ -1,10 +1,18 @@
 <?php
 require('dbconnect.php');
 // $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
-$memos = $db -> query('select * from memos order by id desc');
-if (!$memos) {
+$stmt = $db -> prepare('select * from memos order by id desc limit ?, 5');
+if (!$stmt) {
     die($db -> error);
 }
+
+$page = 5;
+$stmt -> bind_param('i', $page);
+$stmt -> execute();
+// $memos = $db -> query('select * from memos order by id desc limit 0, 5');
+// if (!$memos) {
+//     die($db -> error);
+// }
 ?>
 
 <!DOCTYPE html>
@@ -20,10 +28,11 @@ if (!$memos) {
 
     <p><a href="input.html">新しいメモ</a></p>
 
-    <?php while ($memo = $memos -> fetch_assoc()): ?>
+    <?php $stmt -> bind_result($id, $memo, $created); ?>
+    <?php while ($stmt -> fetch()): ?>
         <div>
-            <h2><a href="memo.php?id=<?php echo $memo['id']; ?>"><?php echo htmlspecialchars(mb_substr($memo['memo'], 0, 50)); ?></a></h2>
-            <time><?php echo htmlspecialchars($memo['created']); ?></time>
+            <h2><a href="memo.php?id=<?php echo $id; ?>"><?php echo htmlspecialchars(mb_substr($memo, 0, 50)); ?></a></h2>
+            <time><?php echo htmlspecialchars($created); ?></time>
         </div>
         <hr>
     <?php endwhile; ?>
