24371b1c0b144b08717e185e8cd9df062aa472f6
kcl215 <98423401+kcl215@users.noreply.github.com>
2022-05-30 02:17:27 +0900

    section4-78 finish

--- memo/index.php
+++ memo/index.php
@@ -1,6 +1,13 @@
 <?php
 require('dbconnect.php');
 // $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
+
+// 最大ページを求める
+$counts = $db -> query('select count(*) as cnt from memos');
+$count = $counts -> fetch_assoc();
+$max_page = floor(($count['cnt'] + 1) / 5 + 1);
+// echo $max_page;
+
 $stmt = $db -> prepare('select * from memos order by id desc limit ?, 5');
 if (!$stmt) {
     die($db -> error);
@@ -54,5 +61,14 @@ $result = $stmt -> execute();
         </div>
         <hr>
     <?php endwhile; ?>
+    
+    <p>
+        <?php if ($page > 1): ?>
+            <a href="?page=<?php echo $page-1; ?>"><?php echo $page-1 ?>ページ目へ</a> | 
+        <?php endif ?>
+        <?php if ($page < $max_page): ?>
+            <a href="?page=<?php echo $page+1; ?>"><?php echo $page+1 ?>ページ目へ</a>
+        <?php endif ?>
+    </p>
 </body>
 </html>
